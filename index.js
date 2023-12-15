09bba62b04cf5806c557499adf2b8ccf2c6ce541
RicardoP <picoricardo3d@outlook.com>
2023-12-14 23:53:30 -0500

    Made IIFE to run a console tic tac toe game
    the IIFE is called gameController it creates two players using
    the factory function CreatePlayer to make player1 and player2
    and inside the gameController the function playGame it uses other
    function like nextPlayer to switch from x and o every other round
    and winCondition function to see if either player has won

--- index.js
+++ index.js
@@ -1,43 +1,97 @@
 const gameBoard = (() => {
-    const boardArray = ['','','','','','','','',''];
+    const _boardArray = ['', '', '', '', '', '', '', '', ''];
 
-    const setBoard = function (num, player) {
-        // sets the board for the initial sign 
-
-
+    const setBoard = (index, player) => {
+        _boardArray[index] = player;
+        render();
     }
 
-    const render = function () {
-    // made to show the changes on the UI 
+    const getBoard = (index) => {
+        return _boardArray[index];
     }
 
-    return {
-        board: boardArray,
+    const resetBoard = () => {
+        for (let i = 0; i < _boardArray.length; i++) {
+            _boardArray[i] = '';
+        }
     }
+
+    const render = () => {
+        console.log(_boardArray);
+    }
+
+    return { setBoard, getBoard, resetBoard, }
 
 })()
 
 
-const CreatePlayer = (plyerSign) => {
-    let sign = plyerSign;
+const CreatePlayer = (playerName, playerSign) => {
+    let player = playerName;
+    let _sign = playerSign;
 
     const getSign = () => {
-        return sign;
+        return _sign;
     };
 
-    return {
-        getSign
+    return { player, getSign }
+};
+
+const gameController = (() => {
+    const player1 = CreatePlayer('Player1', 'X');
+    const player2 = CreatePlayer('Player2', 'O');
+
+    let round = 1;
+    let gameOver = false;
+
+    const playGame = (field) => {
+        gameBoard.setBoard(field, nextPlayer());
+        if (winCondition(field)) {
+            console.log(`${nextPlayer()} Wins`);
+            gameOver = true;
+            return;
+        };
+        round++;
+
+    };
+
+    const nextPlayer = () => {
+        return round % 2 === 1 ? player1.getSign() : player2.getSign();
+    };
+
+    const winCondition = (field) => {
+        const winningArrays = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        return winningArrays
+        .filter((combination) => combination.includes(field))
+        .some((possibleCombination) =>
+        possibleCombination.every(
+            (index) => gameBoard.getBoard(index) === nextPlayer()
+        )
+    );
     }
-}
 
-player1 = CreatePlayer('X');
-console.log(player1);
+return { playGame }
+}) ();
 
-const gameController = () => {
-    // this has to be able to decide how the game goes 
+gameController.playGame(0)
+gameController.playGame(3)
+gameController.playGame(1)
 
-    const player1 = CreatePlayer('X')
-    const player2 = CreatePlayer('O')
+gameController.playGame(3)
+gameController.playGame(2)
 
-}
+
+
+
+
+
 
